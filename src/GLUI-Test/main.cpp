052ffb6eda7b26dbd8cb0ee22f54b7f5530b5226
sharhar <wiishahar@gmail.com>
2016-07-26 14:58:56 -0700

    commit

--- src/GLUI-Test/main.cpp
+++ src/GLUI-Test/main.cpp
@@ -1,12 +1,9 @@
-#include <freetype-gl.h>
 #include <GLUI/Window.h>
 #include <GLUI/GLUI.h>
 #include <GLUI/Input.h>
 #include <GLUI/Listener.h>
 #include <iostream>
 #include <GLFW/glfw3.h>
-#include <ft2build.h>
-#include FT_FREETYPE_H
 
 using namespace glui;
 
@@ -14,13 +11,6 @@ int main() {
 	GLUI::init();
 
 	Window win("Test", 800, 600);
-	
-	FT_Library ft;
-	FT_Init_FreeType(&ft);
-	FT_Face face;
-	if (FT_New_Face(ft, "arial.ttf", 0, &face)) {
-		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
-	}
 
 	std::string s = "hi";
 
